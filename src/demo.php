8df9374b04bb8ef23a9b902ff38603c3c17aa5c8
Adewale Andrade <adewale@oxenti.com.br>
2018-02-02 10:21:31 -0300

    model file

--- src/demo.php
+++ src/demo.php
@@ -6,6 +6,7 @@ include '../vendor/autoload.php';
 use Phpml\Dataset\CsvDataset;
 use Phpml\Dataset\ArrayDataset;
 use Phpml\FeatureExtraction\TokenCountVectorizer;
+//use Phpml\ModelManager;
 use Phpml\Tokenization\WordTokenizer;
 use Phpml\CrossValidation\StratifiedRandomSplit;
 use Phpml\FeatureExtraction\TfIdfTransformer;
@@ -24,12 +25,36 @@ foreach ($dataset->getSamples() as $sample) {
 
 $vectorizer->fit($samples);
 $vectorizer->transform($samples);
+
 $tfIdfTransformer->fit($samples);
 $tfIdfTransformer->transform($samples);
+
 $dataset = new ArrayDataset($samples, $dataset->getTargets());
 $randomSplit = new StratifiedRandomSplit($dataset, 0.1);
 $classifier = new SVC(Kernel::RBF, 10000);
 $classifier->train($randomSplit->getTrainSamples(), $randomSplit->getTrainLabels());
 $predictedLabels = $classifier->predict($randomSplit->getTestSamples());
+//
+//$modelManager = new ModelManager();
+//$modelManager->saveToFile($classifier, './models/categories/category-classifier');
+
 
 echo 'Accuracy: ' . Accuracy::score($randomSplit->getTestLabels(), $predictedLabels);
+
+
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\ModelManager;
+
+$samples = [[1, 3], [1, 4], [2, 4], [3, 1], [4, 1], [4, 2]];
+$labels = ['a', 'a', 'a', 'b', 'b', 'b'];
+
+$classifier = new KNearestNeighbors();
+$classifier->train($samples, $labels);
+
+$filepath = '/path/to/store/the/model';
+$modelManager = new ModelManager();
+$modelManager->saveToFile($classifier, $filepath);
+
+$restoredClassifier = $modelManager->restoreFromFile($filepath);
+$restoredClassifier->predict([3, 2]);
+// return 'b'
